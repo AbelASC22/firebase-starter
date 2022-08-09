cd3921a1393a157c9eb1dc184132956e62309a36
YOUR NAME IN QUOTES <YOUR EMAIL IN QUOTES>
2022-08-09 15:33:15 -0400

    created chatbox (YB BETTER)

--- init-firebase.js
+++ init-firebase.js
@@ -2,9 +2,17 @@
  * @TODO replace the firebaseConfig object below with the one 
  * provided for you by firebase after creating your project!
 */
+
 const firebaseConfig = {
-    
-};
+    apiKey: "AIzaSyDA11TLtH-7EqfWN7TFVP12aIdRPB2v6FY",
+    authDomain: "firemessages-60bc6.firebaseapp.com",
+    databaseURL: "https://firemessages-60bc6-default-rtdb.firebaseio.com",
+    projectId: "firemessages-60bc6",
+    storageBucket: "firemessages-60bc6.appspot.com",
+    messagingSenderId: "1027060578535",
+    appId: "1:1027060578535:web:32e9c6d339de9070758a42"
+  };
+  
 
 // initialize the firebase app with this built-in function
 firebase.initializeApp(firebaseConfig);
